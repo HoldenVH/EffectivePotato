ae33fca46e79e264ccf28a002f8f2f4e51a8be13
aroktim@stuy.edu <aroktim@stuy.edu>
2017-11-15 13:13:09 -0500

    Try to download file but doesn't work????

--- example/text_to_speech2/app.py
+++ example/text_to_speech2/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import urllib2, json, urllib
 import requests
+import shutil
 
 app = Flask(__name__)
 
@@ -11,14 +12,18 @@ def root():
     username = "60365d33-5378-4194-8fb3-2a5f1b88deb0"
     password = "Idq1K6PcNKRs"
     apiurl = "https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize"
-    headers = {"content-type": "application/json", "Accept": "audio/wav" }
+    headers = {"content-type": "application/json", "Accept": "audio/wav", "content-disposition": "attachment; filename=\"resp.wav\""}
     dictionary = {"text": request.args.get("text")}
-    r = requests.get( apiurl, auth=(username,password),
+    r = requests.get( apiurl, auth=(username,password), stream=True,
                       params=dictionary)
-    #print r.content
-    with open("static/resp.wav", "wb") as f:
+    with open('audio.wav', 'wb') as f:
         f.write(r.content)
-        f.close()
+    #print r.content
+    #filename = "static/resp.wav"
+    #with open(filename, "wb") as f:
+    #    f.write(r.content)
+    #    f.close()
+
     statuscode = r.ok
     print statuscode
     return render_template('template.html', text=request.args.get("text"),
